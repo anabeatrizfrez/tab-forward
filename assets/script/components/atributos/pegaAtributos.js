ad6ebae3727d315adeecc1e80bb05ab4afef20df
anabeatrizfrez <anabeatriz.frez@outlook.com>
2026-04-18 14:06:48 -0300

    feat: função que retorna os valores dos atributos

---
+++ assets/script/components/atributos/pegaAtributos.js
@@ -0,0 +1,17 @@
+export function pegaAtributos() {
+
+    const url = document.querySelector('#input_url').value;
+    
+    const form = document.querySelector('#form');
+    const formData = new FormData(form);
+
+    const navegador = document.querySelector('#select_navegador').value
+
+    const dados = {url, navegador};
+
+    formData.forEach((valor, chave) => {
+        dados[chave] = valor;
+    });
+
+    return dados;
+}
