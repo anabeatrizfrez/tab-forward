f1b6702fc542e329916840fd9673175352a3c2fa
anabeatrizfrez <anabeatriz.frez@outlook.com>
2026-04-18 14:04:18 -0300

    feat: função para criar elementos

---
+++ assets/script/components/elementos/criaElementos.js
@@ -0,0 +1,19 @@
+export function criaInput() {
+    return document.createElement('input');
+}
+
+export function criaLabel() {
+    return document.createElement('label');
+}
+
+export function criaSelect() {
+    return document.createElement('select');
+}
+
+export function criaOption() {
+    return document.createElement('option');
+}
+
+export function criaDiv() {
+    return document.createElement('div');
+}
