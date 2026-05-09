00209f1c98bab18416c95d1c78d1f6e1b5d7c45b
anabeatrizfrez <anabeatriz.frez@outlook.com>
2026-05-08 21:53:28 -0300

    style: organizando index.js

--- src/renderer/js/index.js
+++ src/renderer/js/index.js
@@ -1,13 +1,16 @@
+import { identificaSO } from './modules/identificaSO.js';
 import { adicionaElementos } from './ui/adicionaElementos.js';
+import { dataPadrao } from './ui/dataPadrao.js'
+import { recuperaDados } from './ui/guardaDados.js';
 import { limpaAtributos } from './ui/limpaAtributos.js';
 import { pegaAtributos } from './ui/pegaAtributos.js';
 import { programa } from './ui/programa.js';
 import { validaAtributos } from './ui/validaAtributos.js';
 import { verificaCheckBox } from './ui/verificaCheckBox.js';
 import { visualizaAtributos } from './ui/visualizaAtributos.js'
-import { identificaSO } from './modules/identificaSO.js';
 
 document.addEventListener('DOMContentLoaded', () => {
+    recuperaDados();
     identificaSO(); 
     verificaCheckBox();
     adicionaElementos();
@@ -15,5 +18,6 @@ document.addEventListener('DOMContentLoaded', () => {
     pegaAtributos();
     visualizaAtributos();
     limpaAtributos();
+    dataPadrao();
     programa(); 
 });
